4448abe9d88b2e967bb899f4fed5fa4aa369c6fa
Prajwal Malabagi <118988986+Prajwal03Malabagi@users.noreply.github.com>
2026-05-07 13:33:16 +0530

    Add pkg package with UVM includes

---
+++ test/pkg.sv
@@ -0,0 +1,33 @@
+package pkg;
+
+	import uvm_pkg::*;
+	`include "uvm_macros.svh"
+	`include "conf.sv"
+		`include "tx.sv"
+	
+	//`include "confs.sv"
+
+	`include "seqr_s.sv"
+	`include "driver_s.sv"
+	`include "monitor_s.sv"
+	`include "slave_agent.sv"
+	`include "seq_m.sv"
+	`include "seqr_m.sv"
+	`include "driver_m.sv"
+	`include "monitor_m.sv"
+	`include "scb.sv"
+	`include "master_agent.sv"
+	
+
+	//`include "confa.sv"
+		
+	
+
+	
+	//`include "scb.sv"
+	`include "env.sv"
+	`include "test.sv"
+	
+endpackage
+	
+	
